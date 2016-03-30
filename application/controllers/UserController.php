b65a9464b1d21983f26dd60ea3d0b6819297898f
aya-adel <eng.ayaadel92@yahoo.com>
2016-03-30 17:26:11 +0200

    connect the database to the project and check the connection with the first model

---
+++ application/controllers/UserController.php
@@ -0,0 +1,27 @@
+<?php
+
+class UserController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function listusersAction()
+    {
+        $user_model = new Application_Model_User();
+      $th = $user_model->listUsers();
+      var_dump($th);
+    }
+
+
+}
+
+
+
