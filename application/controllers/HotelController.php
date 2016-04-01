f133b5f8c84b072d84a8e9b64ded297658b22972
aya-adel <eng.ayaadel92@yahoo.com>
2016-04-02 01:48:18 +0200

    the model of the hotel and the room with all the crud operation from add list and list with id and delete edit w add

--- application/controllers/HotelController.php
+++ application/controllers/HotelController.php
@@ -12,7 +12,7 @@ class HotelController extends Zend_Controller_Action
     {
         // action body
            $hotel_obj = new Application_Model_Hotel();
-         var_dump($hotel_obj->getAllHotels(1));
+         var_dump($hotel_obj->getAllHotels(6));
     }
 
     public function listAction()
@@ -50,12 +50,53 @@ class HotelController extends Zend_Controller_Action
                 $this->redirect('/hotel/list');
             }
         
+        }
     
     }
+
+    public function deleteAction()
+    {
+        // action body
+        // action body
+         $hotel_obj = new Application_Model_Hotel();
+        $hotel_id = $this->_request->getParam("id");
+         //$country_id = $this->_request->getParam("country_id");
+        $hotel_obj->deleteHotel($hotel_id);
+        $this->redirect("/hotel/list");
+    }
+
+    public function updateAction()
+    {
+        // action body
+         $form = new Application_Form_Hotel(); // hgyb object mn el form w deh elly hmleha b data elly htg3ly mn database 3shan 2sht8l 3leh
+        $hotel_obj = new Application_Model_Hotel(); // deh el object elly hyrg3 data mn database
+        $id = $this->_request->getParam('id'); // deh ana b2os el ide mn ellly htb3tlly lma 2dos 3la button el update 
+        $hotel_got = $hotel_obj->hotelDetail($id); // deh hgyb el details bt3t el user dah b id bt3o 
+        // var_dump($client_got);exit();
+
+        $form->populate($hotel_got[0]); // deh function wzftha 2nha tmlly el form elly 3ndy b data elly htglly bt3t el one user
+        $this->view->form_c = $form; // mfrod hb3t l view el form el gdeda elly htb2a mmlya b data 
+
+        
+        $request = $this->getRequest(); // h3dal el data w hdos submit w btally lzm 2ml function wzftha 2nha bt3ml insert l data gdeda fe database
+        if($request->isPost())
+        {
+            if($form->isValid($request->getPost()))
+            {
+                
+                $hotel_obj->updataHotel($id,$_POST); // deh function wzftha 2nha bt3ml update lzm 23mlha hindel 
+                $this->redirect('/hotel/list'); // deh 3shan yrg3 l nfs sf7t el list 3shan y3rd el data b3d el update 
+
+            }
+        }
     }
 
 
 }
+
+
+
+
 
 
 
