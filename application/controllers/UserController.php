481429af97ea5ce7af37b4bd5bfdc5c3275f0c03
amrfayad <amr.fci2007@gmail.com>
2016-04-02 16:13:43 +0200

    user model and log in

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -10,18 +10,144 @@ class UserController extends Zend_Controller_Action
 
     public function indexAction()
     {
-        // action body
+// action body
     }
 
     public function listusersAction()
     {
-        $user_model = new Application_Model_User();
-      $th = $user_model->listUsers();
-      var_dump($th);
+        $user_obj = new Application_Model_User();
+        $this->view->clients = $user_obj->listUsers();
     }
-    
-    
-}
 
+    public function showUserAction()
+    {
+// action body      
+
+        $user_obj = new Application_Model_User();
+        $user_id = $this->_request->getParam("uid");
+        $user = $user_obj->showUser($user_id);
+        $this->view->ul = $user[0];
+    }
+
+    public function blockUserAction()
+    {
+// action body
+        $user_obj = new Application_Model_User();
+        $user_id = $this->_request->getParam("uid");
+        $user_obj->blockUser($user_id);
+        $this->redirect('/user/listusers');
+    }
+
+    public function editUserAction()
+    {
+// action body
+        $form = new Application_Form_SignUp();
+        $user_obj = new Application_Model_User();
+        $id = $this->_request->getParam('uid');
+        $user_data = $user_obj->showUser($id)[0];
+        $form->populate($user_data);
+        $form->getElement('email')->setAttrib('readonly', 'readonly');
+        $form->getElement('submit')->setName('Update');
+//$form->getElement('email')->setAttrib('disabled','disabled' );
+        $this->view->user_form = $form;
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $user_obj->updateClient($_POST);
+                $this->redirect('/user/listusers');
+            }
+        }
+    }
+
+    public function addUserAction()
+    {
+// action body
+        $form = new Application_Form_SignUp();
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            if ($form->isValid($request->getPost())) {
+                $user_obj = new Application_Model_User();
+                $user_obj->addNewUser($request->getParams());
+                $this->redirect('/user');
+            }
+        }
+        $this->view->user_form = $form;
+    }
+
+    public function logInAction()
+    {
+        // action body
+        // get login form and check for validation
+        $login_form = new Application_Form_Login( );
+        if ($this->_request->isPost()) {
+            if ($login_form->isValid($this->_request->getPost())) {// after check for validation get email and password to start auth
+                $email = $this->_request->getParam('email');
+                $password = $this->_request->getParam('password');
+                // get the default db adapter
+                $db = Zend_Db_Table::getDefaultAdapter();
+                //create the auth adapter
+                $authAdapter = new Zend_Auth_Adapter_DbTable($db, 'user', "email", 'password'); //set the email and password
+                $authAdapter->setIdentity($email);
+                $authAdapter->setCredential($password);
+                //authenticate
+                $result = $authAdapter->authenticate();
+                if ($result->isValid()) {
+                //if the user is valid register his info in session
+                    $auth = Zend_Auth::getInstance(); //if the user is valid register his info in session
+                    $storage = $auth->getStorage();
+                    // write in session email & id & first_name
+                    $storage->write($authAdapter->getResultRowObject(array('email', 'id', 'name')));
+                    // redirect to root index/index
+                    return $this->redirect('/user/listusers');
+                } else {
+                    // if user is not valid send error message to view
+                    //$this->view->error_message = "Invalid Email or Password!";
+                    $login_form->getElement('password')->addError('Invalid Password!');
+                }
+            }
+        }
+        $this->view->login_form = $login_form;
+        
+        
+        
+        $fb = new Facebook\Facebook([
+            'app_id' => '566537100167424', // Replace {app-id} with your app id
+            'app_secret' => '0e72350a1ff5d34e4a0a487f4be811cd',
+            'default_graph_version' => 'v2.2',
+        ]);
+        $helper = $fb->getRedirectLoginHelper();
+        $loginUrl = $helper->getLoginUrl($this->view->serverUrl() .
+                $this->view->baseUrl() . '/user/fpauth');
+        $this->view->facebook_url = $loginUrl;
+    }
+
+    public function logOutAction()
+    {
+        
+         // action body
+        $auth = Zend_Auth::getInstance();
+        $auth->clearIdentity();
+        return $this->redirect('/user/log-in');
+    }
+
+    public function activateUserAction()
+    {
+// action body
+        $user_obj = new Application_Model_User();
+        $user_id = $this->_request->getParam("uid");
+        $user_obj->activaetUser($user_id);
+        $this->redirect('/user/listusers');
+    }
+
+    public function fpauthAction()
+    {
+        // action body
+        
+        
+        
+    }
+
+
+}
 
 
---
+++ application/forms/SignUp.php
@@ -0,0 +1,91 @@
+<?php
+// id 	name 	email 	password
+//  	is_enable 	tel 	gender 
+
+class Application_Form_SignUp extends Zend_Form
+{
+
+    public function init()
+    {
+        /* Form Elements & Other Definitions Here ... */
+        
+        $this->setMethod('POST');
+        $id = new Zend_Form_Element_Hidden('id');
+        $name = new Zend_Form_Element_Text('name');
+        $name->setLabel('Your Name: ');
+        $name->setAttribs(Array(
+            'placeholder' => 'Example: Mohamed',
+            'class' => 'form-control'
+        ));
+        $name->setRequired();
+        
+        $email = new Zend_Form_Element_Text('email');
+        $email->setLabel('Email Address');
+        $email->setAttribs(Array(
+            'placeholder' => 'Example: Ali@yahoo.com',
+            'class' => 'form-control'
+        ));
+        $email->setRequired();
+        $db = Zend_Db_Table::getDefaultAdapter();
+        $validator = new Zend_Validate_Db_NoRecordExists(
+                array(
+                    'table' => 'user',
+                    'field' => 'email',
+                    'adapter' => $db
+                )
+        );
+        $email->addValidator('EmailAddress', TRUE);
+        $email->addValidator($validator,TRUE);
+        $password = new Zend_Form_Element_Password('password');
+        $password->setLabel('Password: ');
+        $password->setAttribs(Array(
+            'class' => 'form-control'
+        ));
+        $password->setRequired();
+        $confirm = new Zend_Form_Element_Password('confirm');
+        $confirm->setLabel('Retype-Password: ');
+        $confirm->setAttribs(Array(
+            'class' => 'form-control'
+        ));
+        $confirm->setRequired();
+        $v = new Zend_Validate_Identical("password");
+        $confirm->addValidator($v);
+        $gender = new Zend_Form_Element_Select('gender');
+        $gender->setRequired();
+        $gender->addMultiOption('male', 'Male')->
+                addMultiOption('female', 'Female')->
+                addMultiOption('non', 'Prefer not to mention');
+        $gender->setAttrib('class', 'form-control');
+        $tel = new Zend_Form_Element_Text('tel');
+        $tel->setLabel('Tel Number : ');
+        $tel->setAttrib('class', 'form-control');
+        // submit
+        $submit = new Zend_Form_Element_Submit('submit');
+        $submit->setValue('Save');
+        $submit->setAttrib('class', 'btn btn-success');
+
+        //reset
+        $reset = new Zend_Form_Element_Reset('reset');
+        $reset->setValue('Reset');
+        $reset->setAttrib('class', 'btn btn-danger');
+
+
+        // Add Element to my form
+        $this->addElements(array(
+            $id,
+            $name,
+            $email,
+            $password,
+            $confirm,
+            $gender,
+            $tel,
+            $submit,
+            $reset
+        ));
+        
+        
+    }
+
+
+}
+
